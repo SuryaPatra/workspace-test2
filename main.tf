8876063ba6e1485d538e9ca16efe6a3eef2310ea
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-07-24 16:05:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,13 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+resource "aws_instance" "example" {
+    ami           = "ami-086b16d6badeb5716"
+    instance_type = "t2.micro"
+    key_name = "workstation"
+}
 
-resource "aws_instance" "web" {
-  ami           = "ami-003634241a8fcdec0"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+output "instance_key" {
+    value = "${aws_instance.example.key_name}"
 }
