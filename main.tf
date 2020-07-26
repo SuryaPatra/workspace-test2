c4edde6029bbb46ce08a80e01e261cf1e2b191a4
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-07-26 17:44:20 +0530

    updated key name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-086b16d6badeb5716"
     instance_type = "t2.micro"
-    key_name = "workstation"
+    key_name = "workstation-key"
 }
 
 
